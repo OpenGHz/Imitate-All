ca3134110c99c6796b637810df7d98dd777ccbc9
Haizhou Ge <1352674740@qq.com>
2024-09-29 23:35:54 +0800

    add orbbec camera recoder

--- data_process/record_realsense.py
+++ data_process/record_realsense.py
@@ -1,7 +1,5 @@
 import time
-import pyrealsense2 as rs
 import numpy as np
-import cv2
 import os
 from pathlib import Path
 from PIL import Image
@@ -9,7 +7,38 @@ from concurrent.futures import ThreadPoolExecutor
 from collections import OrderedDict
 import subprocess
 import argparse
-from typing import Optional
+from typing import Optional, Union, Any
+
+
+iMPORT_ERRORS = []
+try:
+    import pyrealsense2 as rs
+except ImportError:
+    iMPORT_ERRORS.append("pyrealsense2")
+    print("pyrealsense2 not found. You can install it with `pip install pyrealsense2`")
+
+try:
+    import cv2
+except ImportError:
+    iMPORT_ERRORS.append("cv2")
+    print("cv2 not found. You can install it with `pip install opencv-python`")
+
+try:
+    from pyorbbecsdk import (
+        Config,
+        VideoStreamProfile,
+        OBError,
+        OBSensorType,
+        FormatConvertFilter,
+        VideoFrame,
+        OBFormat,
+        OBConvertFormat,
+        Pipeline,
+        FrameSet,
+    )
+except Exception as e:
+    iMPORT_ERRORS.append("pyorbbecsdk")
+    print(f"Error with pyorbbecsdk package: {e}")
 
 
 def save_images_concurrently(
@@ -87,12 +116,12 @@ def encode_video_frames(
         )
 
 
-class Camera(object):
+class RealsenseCamera(object):
     """
-    realsense相机处理类
+    realsense 相机处理类
     """
 
-    def __init__(self, width=1280, height=720, fps=30):
+    def __init__(self, width, height, fps):
         self.width = width
         self.height = height
         self.pipeline = rs.pipeline()
@@ -129,6 +158,71 @@ class Camera(object):
         self.pipeline.stop()
 
 
+class OrbbecCamera(object):
+    """
+    orbbec 相机处理类
+    """
+
+    def __init__(self, width, height="auto", fps="max") -> None:
+        self.config = Config()
+        self.pipeline = Pipeline()
+        profile_list = self.pipeline.get_stream_profile_list(OBSensorType.COLOR_SENSOR)
+        assert width in [640, 1280, 1920], "Width must be 640, 1280, or 1920"
+        fps = 30 if fps == "max" else fps
+        try:
+            color_profile: VideoStreamProfile = profile_list.get_video_stream_profile(width, 0, OBFormat.RGB, fps)
+        except OBError as e:
+            print(e)
+            color_profile = profile_list.get_default_video_stream_profile()
+            # print("color profile: ", color_profile)
+        self.config.enable_stream(color_profile)
+        self.pipeline.start(self.config)
+
+    def get_color_image(self):
+        frames: FrameSet = self.pipeline.wait_for_frames(100)
+        if frames is None:
+            return None
+        color_frame = frames.get_color_frame()
+        if color_frame is None:
+            return None
+        # covert to RGB format
+        color_image = self.frame_to_bgr_image(color_frame)
+        return color_image
+
+    @staticmethod
+    def determine_convert_format(frame: VideoFrame):
+        if frame.get_format() == OBFormat.I420:
+            return OBConvertFormat.I420_TO_RGB888
+        elif frame.get_format() == OBFormat.MJPG:
+            return OBConvertFormat.MJPG_TO_RGB888
+        elif frame.get_format() == OBFormat.YUYV:
+            return OBConvertFormat.YUYV_TO_RGB888
+        elif frame.get_format() == OBFormat.NV21:
+            return OBConvertFormat.NV21_TO_RGB888
+        elif frame.get_format() == OBFormat.NV12:
+            return OBConvertFormat.NV12_TO_RGB888
+        elif frame.get_format() == OBFormat.UYVY:
+            return OBConvertFormat.UYVY_TO_RGB888
+        else:
+            return None
+
+    @classmethod
+    def frame_to_rgb_frame(cls, frame: VideoFrame) -> Union[Optional[VideoFrame], Any]:
+        if frame.get_format() == OBFormat.RGB:
+            return frame
+        convert_format = cls.determine_convert_format(frame)
+        if convert_format is None:
+            print("Unsupported format")
+            return None
+        print("covert format: {}".format(convert_format))
+        convert_filter = FormatConvertFilter()
+        convert_filter.set_format_convert_format(convert_format)
+        rgb_frame = convert_filter.process(frame)
+        if rgb_frame is None:
+            print("Convert {} to RGB failed".format(frame.get_format()))
+        return rgb_frame
+
+
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
@@ -144,6 +238,7 @@ if __name__ == "__main__":
     parser.add_argument("--fast_decode", type=int, default=0)
     parser.add_argument("--log_level", type=str, default="error")
     parser.add_argument("--overwrite", action="store_true")
+    parser.add_argument("-ct", "--camera_type", type=str, default="realsense")
     args = parser.parse_args()
     # 视频保存路径
     imgs_dir = args.imgs_dir
@@ -175,18 +270,24 @@ if __name__ == "__main__":
     wr_camera_colordepth = cv2.VideoWriter(
         video_depthcolor_camera_path, mp4, fps, (w, h), isColor=True
     )
-
-    cam = Camera(w, h, fps)
+    # 初始化相机
+    if args.camera_type == "orbbec":
+        cam = OrbbecCamera(w, h, fps)
+    elif args.camera_type == "realsense":
+        cam = RealsenseCamera(w, h, fps)
     print("录制视频请按: s, 保存视频或退出请按：q")
     flag_V = 0
     cv2.namedWindow("RealSense", cv2.WINDOW_AUTOSIZE)
     while True:
         # 读取图像帧，包括RGB图和深度图
-        color_image, depth_image, colorizer_depth = cam.get_frame()
-        # 深度图数据格式转换，uint16→uint8
-        depth_colormap = cv2.applyColorMap(
-            cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET
-        )
+        if args.camera_type == "orbbec":
+            color_image = cam.get_color_image()
+        elif args.camera_type == "realsense":
+            color_image, depth_image, colorizer_depth = cam.get_frame()
+            # 深度图数据格式转换，uint16→uint8
+            depth_colormap = cv2.applyColorMap(
+                cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET
+            )
         # images = np.hstack((color_image, depth_colormap))
         cv2.imshow("RealSense", color_image)
 
@@ -195,7 +296,9 @@ if __name__ == "__main__":
             flag_V = 1
             print("...录制视频中...")
         elif key == ord("q"):
-            print(f"Saving images concurrently to {imgs_dir}, frames:", len(color_images))
+            print(
+                f"Saving images concurrently to {imgs_dir}, frames:", len(color_images)
+            )
             images_array = np.array(color_images)
             save_images_concurrently(
                 images_array, out_dir=Path(imgs_dir), max_workers=4
