2a288c30c223c892ee39f1eaa3affe13c005dc86
Haizhou Ge <1352674740@qq.com>
2024-11-23 21:45:58 +0800

    update mmk2

---
+++ envs/airbot_mmk_env_client.py
@@ -0,0 +1,96 @@
+import time
+import numpy as np
+import collections
+import dm_env
+from typing import List
+from robots.airbots.airbot_mmk2.mmk2_client import AirbotMMK2 as MMK2Robot
+
+
+class AirbotMmkEnv:
+
+    def __init__(
+        self,
+        robot_instances: List[MMK2Robot] = None,
+        camera_names=None,
+    ):
+        self.mmk2_robots = [MMK2Robot()]
+        self.robot_num = len(self.mmk2_robots)
+
+        name_converter = {
+            "0": "camera_head",
+            "1": "camera_left",
+            "2": "camera_right",
+        }
+        self.name_converter_rev = {v: k for k, v in name_converter.items()}
+        camera_names = ["0"]
+        for i in range(len(camera_names)):
+            camera_names[i] = name_converter[camera_names[i]]
+        # self.image_recorder = ImageRecorderRos(camera_names)
+        self.all_joints_num = 7 * 2  # + 2 + 1
+        print("AIRBOT MMK Real Environment Created.")
+
+    def _get_images(self):  # 获取图像
+        # images_dic = self.image_recorder.get_images()
+        # images = {}
+        # for key, value in images_dic.items():
+        # #     images[self.name_converter_rev[key]] = value
+        # for robot in self.mmk2_robots:
+        #     images=robot.get_image()
+        return {"0": self.mmk2_robots[0].get_image()}
+
+    def get_reward(self):  # 获取奖励
+        return 0
+
+    def reset(self, sleep_time=0):  # 重置环境
+        for robot in self.mmk2_robots:
+            robot.reset()
+
+        return dm_env.TimeStep(
+            step_type=dm_env.StepType.FIRST,
+            reward=self.get_reward(),
+            discount=None,
+            observation=self._get_observation(),
+        )
+
+    def step(
+        self,
+        action,
+        sleep_time=0,
+        arm_vel=0,
+    ):
+        for index, robot in enumerate(self.mmk2_robots):
+            jn = robot.all_joints_num
+            robot.set_target_states(
+                action[jn * index : jn * (index + 1)],
+                [arm_vel],
+                False,
+            )
+        time.sleep(sleep_time)
+        obs = self._get_observation()
+        return dm_env.TimeStep(
+            step_type=dm_env.StepType.MID,
+            reward=self.get_reward(),
+            discount=None,
+            observation=obs,
+        )
+
+    def _get_state(self):
+        """17 dof: (6 arm joints + 1 gripper joint) * 2 + 2 head joints + 1 spine joint"""
+        qpos = []
+        for robot in self.mmk2_robots:
+            qpos.append(robot.get_current_states())
+        return np.hstack(qpos)
+
+    def _get_observation(self):
+        obs = collections.OrderedDict()
+        obs["qpos"] = self._get_state()
+        obs["images"] = self._get_images()
+        return obs
+
+
+def make_env(
+    robot_instance=None,
+    cameras=None,
+):
+    env = AirbotMmkEnv(robot_instance, cameras)
+    return env
---
+++ robots/airbots/airbot_mmk2/mmk2_client.py
@@ -0,0 +1,81 @@
+import sys
+from mmk2_client import MMK2
+from typing import Dict
+import cv2
+
+
+class AirbotMMK2(object):
+    def __init__(self, config: Dict[str, dict] = None):
+        self.robot = MMK2("mmk2", -1, "192.168.11.200")
+        self.all_joints_num = 14
+
+    def set_target_states(self, qpos, qvel=None, blocking=False):
+        if not blocking:
+            self.robot.set_arms_servo(qpos[0:6], "l", False)
+            self.robot.set_arms_servo(qpos[7:13], "r", False)
+            self.robot.set_gripper(qpos[6], "l")
+            self.robot.set_gripper(qpos[13], "r")
+            # print(qpos)
+        else:
+            # TODO: change the outer env to use the reset funtion for reseting
+            self.reset()
+
+    def get_image(self):
+        image = self.robot.get_image("head", "color")
+        return image
+
+    def get_current_states(self) -> list:
+        all_joint = self.robot.get_all_joint_states()
+        return (
+            list(all_joint[2])
+            + list(all_joint[4])
+            + list(all_joint[3])
+            + list(all_joint[5])
+        )
+
+    def reset(self) -> list:
+        self.robot.set_arms(
+            [
+                -0.2588311731815338,
+                -0.7001983523368835,
+                0.9470130205154419,
+                1.9457160234451294,
+                -1.099603295326233,
+                -0.41065841913223267,
+            ],
+            "l",
+            False,
+        )
+        self.robot.set_arms(
+            [
+                0.6288624405860901,
+                -0.6555657386779785,
+                0.9355688095092773,
+                -1.9705119132995605,
+                1.1411840915679932,
+                0.368314653635025,
+            ],
+            "r",
+            False,
+        )
+        self.robot.execute_trajectory(True)
+        self.robot.set_head([0.03, -0.95])
+        self.robot.set_spine(0.042)
+
+        return self.get_current_states()
+
+
+def main(args=object):
+    robot = AirbotMMK2()
+    # robot.set_target_states([0,0,0,0,0,0,1,0,0,0,0,0,0,1])
+    # cap=cv2.imread(robot.get_image())
+
+    cv2.imshow("Video", robot.get_image())
+    cv2.waitKey(0)
+    # 释放视频捕获对象并关闭所有窗口
+    # cap.release()
+    # cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    main()
