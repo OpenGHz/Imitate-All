e688b812a689adb239fdf57822fbf30bacd4158e
Haizhou Ge <1352674740@qq.com>
2024-12-11 19:28:38 +0800

    add env makes as examples

---
+++ configurations/task_configs/config_tools/env_makers.py
@@ -0,0 +1,16 @@
+def make_airbot_play3_env(config: dict):
+    from envs.airbot_play_3_with_cam import AIRBOTPlayWithCameraEnv
+
+    return AIRBOTPlayWithCameraEnv(config["env_config_path"])
+
+
+def make_airbot_tok_2_env(config: dict):
+    from envs.airbot_tok_2_env import AIRBOTTOKEnv
+
+    return AIRBOTTOKEnv(config["env_config_path"])
+
+
+def make_airbot_mmk2_env(config: dict):
+    from envs.airbot_mmk_env import make_env
+
+    return make_env(config)
