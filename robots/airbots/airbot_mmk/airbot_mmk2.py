910a466de21e98efb110237326d905c5e611f624
Haizhou Ge <1352674740@qq.com>
2024-12-13 14:34:42 +0800

    fix eval get action

--- robots/airbots/airbot_mmk/airbot_mmk2.py
+++ robots/airbots/airbot_mmk/airbot_mmk2.py
@@ -135,18 +135,19 @@ class AIRBOTMMK2(object):
                 all_joints, self.joint_names[comp]
             )
             data[f"observation/{comp.value}/joint_position"] = joint_states
-            if comp in MMK2ComponentsGroup.ARMS:
-                arm_jn = JointNames().__dict__[comp.value]
-                comp_eef = comp.value.replace("arm", "eef")
-                eef_jn = JointNames().__dict__[comp_eef]
-                js = self.robot.get_listened(self._comp_action_topic[comp])
-                jq = self.robot.get_joint_values_by_names(js, arm_jn + eef_jn)
-                data[f"action/{comp.value}/joint_position"] = jq[:-1]
-                # the eef joint is in arms
-                data[f"action/{comp_eef}/joint_position"] = jq[-1:]
-            elif comp in MMK2ComponentsGroup.HEAD_SPINE:
-                jq = list(self.robot.get_listened(self._comp_action_topic[comp]).data)
-                data[f"action/{comp.value}/joint_position"] = jq
+            if self.config.demonstrate:
+                if comp in MMK2ComponentsGroup.ARMS:
+                    arm_jn = JointNames().__dict__[comp.value]
+                    comp_eef = comp.value.replace("arm", "eef")
+                    eef_jn = JointNames().__dict__[comp_eef]
+                    js = self.robot.get_listened(self._comp_action_topic[comp])
+                    jq = self.robot.get_joint_values_by_names(js, arm_jn + eef_jn)
+                    data[f"action/{comp.value}/joint_position"] = jq[:-1]
+                    # the eef joint is in arms
+                    data[f"action/{comp_eef}/joint_position"] = jq[-1:]
+                elif comp in MMK2ComponentsGroup.HEAD_SPINE:
+                    jq = list(self.robot.get_listened(self._comp_action_topic[comp]).data)
+                    data[f"action/{comp.value}/joint_position"] = jq
         return data
 
     def capture_observation(self):
