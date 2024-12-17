fde62791dfa235f2fa4ccc16503737c106feb777
Haizhou Ge <1352674740@qq.com>
2024-12-17 22:28:05 +0800

    fix pad

--- data_process/convert_all.py
+++ data_process/convert_all.py
@@ -451,24 +451,26 @@ def save_dict_to_hdf5(data: dict, target_path: str, pad_max_len: Optional[int] =
             # padding the data with the last N values
             if pad_max_len is not None:
 
-                def pad(value):
+                def pad(value, pad_max_len):
                     raw_len = len(value)
                     size_to_pad = pad_max_len - raw_len
                     # print(f"raw_len = {raw_len}, size_to_pad = {size_to_pad}")
                     pad_times = size_to_pad // raw_len
                     size_to_pad = size_to_pad % raw_len
+
                     # print(f"size_to_pad = {size_to_pad}, pad_times = {pad_times}")
-                    if size_to_pad > 0:
+                    if pad_times > 0:
                         value *= (pad_times + 1)
+                    if size_to_pad > 0:
                         value += value[-size_to_pad:]
                         # print(f"len(value) = {len(value)}")
                     return value
 
                 if is_dict_value:
                     for i, v in enumerate(value):
-                        value[i] = pad(v)
+                        value[i] = pad(v, pad_max_len)
                 else:
-                    value = pad(value)
+                    value = pad(value, pad_max_len)
             root.create_dataset(
                 key, data=np.array(value, dtype=array_type), dtype=dtype, chunks=chunks
             )
