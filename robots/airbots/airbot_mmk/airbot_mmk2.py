715789edb141acc39c7bfe5afb1a4be75f12d5f2
Haizhou Ge <1352674740@qq.com>
2025-01-03 11:39:55 +0800

    fix mmk2 replay

--- robots/airbots/airbot_mmk/airbot_mmk2.py
+++ robots/airbots/airbot_mmk/airbot_mmk2.py
@@ -105,28 +105,34 @@ class AIRBOTMMK2(object):
         # logger.info("AIRBOTMMK2 is ready")
         self.reset()
 
+    def _move_by_traj(self, goal: dict):
+        goal.update(
+            {
+                MMK2Components.HEAD: JointState(position=[0, -1.0]),
+                MMK2Components.SPINE: JointState(position=[0.15]),
+            }
+        )
+        if self.config.demonstrate:
+            # TODO: since the arms and eefs are controlled by the teleop bag
+            for comp in MMK2ComponentsGroup.ARMS_EEFS:
+                goal.pop(comp)
+        if goal:
+            self.robot.set_goal(goal, TrajectoryParams())
+            self.robot.set_goal(goal, ForwardPositionParams())
+        
+        return goal
+
     def reset(self, sleep_time=0):
         if self.config.default_action is not None:
             goal = self._action_to_goal(self.config.default_action)
             # logger.info(f"Reset to default action: {self.config.default_action}")
             # logger.info(f"Reset to default goal: {goal}")
             # TODO: hard code for spine&head control
-            # goal.update(
-            #     {
-            #         MMK2Components.HEAD: JointState(position=[0, -1.0]),
-            #         MMK2Components.SPINE: JointState(position=[0.15]),
-            #     }
-            # )
-            if self.config.demonstrate:
-                # TODO: since the arms and eefs are controlled by the teleop bag
-                for comp in MMK2ComponentsGroup.ARMS_EEFS:
-                    goal.pop(comp)
-            if goal:
-                self.robot.set_goal(goal, TrajectoryParams())
-                self.robot.set_goal(goal, ForwardPositionParams())
+            self._move_by_traj(goal)
         else:
             logger.warning("No default action is set.")
         time.sleep(sleep_time)
+        self.enter_servo_mode()
 
     def send_action(self, action, wait=False):
         goal = self._action_to_goal(action)
@@ -139,8 +145,12 @@ class AIRBOTMMK2(object):
         #     MMK2Components.RIGHT_EEF: TrajectoryParams(),
         # }
         # param = MoveServoParams(header=self.robot.get_header())
-        param = ForwardPositionParams()
-        self.robot.set_goal(goal, param)
+        if self.traj_mode:
+            self._move_by_traj(goal)
+        else:
+            # param = ForwardPositionParams()
+            param = MoveServoParams(header=self.robot.get_header())
+            self.robot.set_goal(goal, param)
 
     def get_low_dim_data(self):
         data = {}
@@ -211,8 +221,16 @@ class AIRBOTMMK2(object):
 
     def low_dim_to_action(self, low_dim: dict, step: int) -> list:
         action = []
+        # logger.info(low_dim.keys())
         for comp in self.components:
-            action.extend(low_dim[f"action/{comp.value}/joint_position"][step])
+            # action.extend(low_dim[f"action/{comp.value}/joint_position"][step])
+            # old version
+            if comp in MMK2ComponentsGroup.ARMS_EEFS:
+                pos_comp = comp.value.split("_")
+                key = f"{pos_comp[1]}/{pos_comp[0]}"
+            else:
+                key = comp.value
+            action.extend(low_dim[f"action/{key}/joint_position"][step])
         return action
 
     def _set_mode(self, mode):
@@ -234,9 +252,11 @@ class AIRBOTMMK2(object):
         return goal
 
     def enter_traj_mode(self):
+        self.traj_mode = True
         logger.info("Enter traj mode")
 
     def enter_servo_mode(self):
+        self.traj_mode = False
         logger.info("Enter servo mode")
 
 
