bc19b248ca7b061b34bdcd0c69d30b7540903975
Haizhou Ge <1352674740@qq.com>
2025-03-15 12:09:54 +0800

    support for airbot play 4.4.5

---
+++ envs/airbot_play_4_with_cam.py
@@ -0,0 +1,72 @@
+import time
+import collections
+import dm_env
+from typing import List, Union
+from robots.airbots.airbot_play.airbot_play_4 import AIRBOTPlay
+from robots.common import make_robot_from_yaml
+
+
+class AIRBOTPlayWithCameraEnv(object):
+
+    def __init__(
+        self,
+        config_paths: Union[List[str], str],
+    ):
+        self.robots: List[AIRBOTPlay] = []
+        if isinstance(config_paths, str):
+            config_paths = [config_paths]
+        for cfg in config_paths:
+            robot = make_robot_from_yaml(cfg)
+            assert isinstance(robot, AIRBOTPlay)
+            self.robots.append(robot)
+        print("robot number:", len(self.robots))
+        self._all_joints_num = [7] * len(self.robots)
+
+    def set_reset_position(self, reset_position):
+        self._all_joints_num = []
+        for i, robot in enumerate(self.robots):
+            robot.config.start_arm_joint_position = reset_position[i * 6 : (i + 1) * 6]
+            robot.config.start_eef_joint_position = reset_position[(i + 1) * 6]
+            self._all_joints_num.append(7)
+
+    def _get_obs(self):
+        obs = collections.OrderedDict()
+        obs["qpos"] = []
+        obs["images"] = {}
+        for robot in self.robots:
+            raw_obs = robot.capture_observation()
+            low_dim = raw_obs["low_dim"]
+            obs["qpos"].extend(
+                low_dim["observation/arm/joint_position"]
+                + low_dim["observation/eef/joint_position"]
+            )
+            for name in robot.cameras:
+                assert name not in obs["images"], f"Duplicate camera name: {name}"
+                obs["images"][name] = raw_obs[f"observation.images.{name}"]
+
+        return dm_env.TimeStep(
+            step_type=dm_env.StepType.FIRST,
+            reward=0,
+            discount=None,
+            observation=obs,
+        )
+
+    def reset(self, sleep_time=0):
+        for robot in self.robots:
+            robot.reset()
+            robot.enter_servo_mode()
+        time.sleep(sleep_time)
+        return self._get_obs()
+
+    def step(
+        self,
+        action,
+        sleep_time=0,
+        get_obs=True,
+    ):
+        for index, jn in enumerate(self._all_joints_num):
+            one_action = action[jn * index : jn * (index + 1)]
+            self.robots[index].send_action(one_action)
+        time.sleep(sleep_time)
+        obs = self._get_obs() if get_obs else None
+        return obs
---
+++ robots/airbots/airbot_play/airbot_play_4.py
@@ -0,0 +1,173 @@
+from dataclasses import dataclass, field, replace
+import time
+from habitats.common.robot_devices.cameras.utils import Camera
+from typing import Dict, Optional, List
+from airbot_py.airbot_play import AirbotPlay
+
+
+
+@dataclass
+class AIRBOTPlayConfig(object):
+    arm_type: str = "play_short"
+    end_effector: str = "G2"
+    port: int = 50000
+    start_arm_joint_position: List[float] = field(
+        default_factory=lambda: [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+    )
+    start_eef_joint_position: float = 0.0
+    # ONLINE_TRAJ, ONLINE_IDLE, ONLINE_SERVO, DEMONSTRATE_PREP
+    default_robot_mode: str = "ONLINE_IDLE"
+    cameras: dict = field(default_factory=lambda: {})
+    display: bool = False
+
+    def __post_init__(self):
+        assert self.default_robot_mode in [
+            "ONLINE_TRAJ",
+            "ONLINE_IDLE",
+            "ONLINE_SERVO",
+            "DEMONSTRATE_PREP",
+        ]
+
+
+class AIRBOTPlay(object):
+    def __init__(self, config: Optional[AIRBOTPlayConfig] = None, **kwargs) -> None:
+        if config is None:
+            config = AIRBOTPlayConfig()
+        self.config = replace(config, **kwargs)
+        self.cameras: Dict[str, Camera] = self.config.cameras
+        self.logs = {}
+        self.__init()
+        self._state_mode = "active"
+        self._exited = False
+
+    def __init(self):
+        args = self.config
+        # Connect the cameras
+        for name in self.cameras:
+            self.cameras[name].connect()
+        # Connect the robot
+        self.robot = AirbotPlay(
+                    port = args.port
+                )
+        time.sleep(0.3)
+        self.reset()
+
+    def reset(self):
+        args = self.config
+        robot = self.robot
+        # set to traj mode
+        if args.arm_type != "replay" and robot.get_current_state() != "ONLINE_TRAJ":
+            assert robot.online_mode(), "online idle mode failed"
+            # assert robot.online_traj_mode(), "online traj mode failed"
+        time.sleep(0.3)
+        # go to start position
+        if args.arm_type != "replay":
+            if args.start_arm_joint_position is not None:
+                assert robot.set_target_joint_q(
+                    args.start_arm_joint_position, blocking=True
+                ), "set target joint q failed"
+            if args.start_eef_joint_position is not None and robot.params["eef_type"] not in ["none", "E2B"]:
+                assert robot.set_target_end_accurate(
+                    args.start_eef_joint_position, blocking=False
+                ), "set target end failed"
+            # enter default mode
+            if args.default_robot_mode == "ONLINE_TRAJ":
+                self.enter_traj_mode()                          
+            elif args.default_robot_mode == "ONLINE_IDLE":
+                self.enter_active_mode()                        
+            elif args.default_robot_mode == "ONLINE_SERVO":
+                self.enter_servo_mode()                        
+            else:
+                raise ValueError(
+                    f"Invalid default robot mode: {args.default_robot_mode}"
+                )
+
+        self._state_mode = "active"
+
+    def enter_traj_mode(self):
+        self.enter_active_mode()
+        if self.config.arm_type == "replay":
+            return
+        # else:
+        #     assert self.robot.online_traj_mode(), "online traj mode failed"
+        time.sleep(0.5)
+        self._state_mode = "active"
+
+    def enter_active_mode(self):
+        if self.config.arm_type == "replay":
+            return
+        else:
+            assert self.robot.online_mode(), "online idle mode failed"
+        self._state_mode = "active"
+
+    def enter_passive_mode(self):
+        if self.config.arm_type == "replay":
+            return
+        else:
+            assert self.robot.manual_mode(), "demonstrate start mode failed"
+        self._state_mode = "passive"
+
+    def enter_servo_mode(self):
+        self.enter_active_mode()
+        if self.config.arm_type == "replay":
+            return
+        # else:
+        #     assert self.robot.online_servo_mode(), "online_servo_mode mode failed"          # 没有online_servo_mode
+        self._state_mode = "active"
+
+    def send_action(self, action, wait=False):
+        assert self._state_mode == "active", "Robot is not in active mode"
+        if self.config.arm_type == "replay":
+            return
+        else:
+            assert self.robot.set_target_joint_q(
+                action[:6], blocking=wait, vel=0.5, use_planning=False
+            ), "set target joint q failed"
+            if self.robot.params["eef_type"] not in ["none", "E2B"]:
+                # assert self.robot.set_target_end_accurate(action[6]), "set target end failed"
+                self.robot.set_target_end_accurate(action[6], blocking=wait)
+        return action
+
+    def get_low_dim_data(self):
+        data = {}
+        data["/time"] = time.time()
+        pose = self.robot.get_current_pose()
+        data["observation/arm/joint_position"] = list(self.robot.get_current_joint_q())
+        data["observation/eef/joint_position"] = [self.robot.get_current_end()]
+        data["observation/eef/pose"] = pose[0] + pose[1]  # xyz + quat(xyzw)
+        return data
+
+    def capture_observation(self):
+        """The returned observations do not have a batch dimension."""
+        obs_act_dict = {}
+        # Capture images from cameras
+        images = {}
+        for name in self.cameras:
+            before_camread_t = time.perf_counter()
+            images[name] = self.cameras[name].async_read()
+            # images[name] = torch.from_numpy(images[name])
+            obs_act_dict[f"/time/{name}"] = time.time()
+            self.logs[f"read_camera_{name}_dt_s"] = self.cameras[name].logs[
+                "delta_timestamp_s"
+            ]
+            self.logs[f"async_read_camera_{name}_dt_s"] = (
+                time.perf_counter() - before_camread_t
+            )
+
+        low_dim_data = self.get_low_dim_data()
+
+        # Populate output dictionnaries and format to pytorch
+        obs_act_dict["low_dim"] = low_dim_data
+        for name in self.cameras:
+            obs_act_dict[f"observation.images.{name}"] = images[name]
+        return obs_act_dict
+
+    def exit(self):
+        assert not self._exited, "Robot already exited"
+        for name in self.cameras:
+            self.cameras[name].disconnect()
+        self._exited = True
+        print("Robot exited")
+
+    def get_state_mode(self):
+        return self._state_mode
