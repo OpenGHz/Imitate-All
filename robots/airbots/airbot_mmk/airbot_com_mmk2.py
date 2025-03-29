4413a977333ae91a21f3902b8e3ac71bc16cc774
Haizhou Ge <1352674740@qq.com>
2025-03-29 22:09:56 +0800

    update

--- robots/airbots/airbot_mmk/airbot_com_mmk2.py
+++ robots/airbots/airbot_mmk/airbot_com_mmk2.py
@@ -127,7 +127,7 @@ class AIRBOTMMK2(object):
             self.robot.set_goal(goal, TrajectoryParams())
             # logger.info(f"Move by trajectory time: {time.time() - start}")
             self.robot.set_goal(goal, ForwardPositionParams())
-            logger.info(f"Move by trajectory: {goal}")
+            # logger.info(f"Move by trajectory: {goal}")
         return goal
 
     def reset(self, sleep_time=0):
